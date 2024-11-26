d911308b90ce6228a4841a2377c04a1ffe954406
rahma <fijriatirahmaturrizqi@gmail.com>
2024-11-26 22:38:43 +0700

    update menambahkan penjelasan fungsi

--- DeretDescendingRekursif.java
+++ DeretDescendingRekursif.java
@@ -1,6 +1,7 @@
 public class DeretDescendingRekursif {
     public static void main(String[] args) {
-        cetakDeretRekursif(5);
+        System.out.print("cetak deret rekursif: ");
+        cetakDeretRekursif(7);
         System.out.println();
         cetakDeretIteratif(7);
     }
@@ -13,6 +14,7 @@ public class DeretDescendingRekursif {
     }
 
     public static void cetakDeretIteratif(int n) {
+        System.out.print("cetak deret iteratif: ");
         for (int i = n; i >= 0; i--) {
             System.out.print(i);
         }
